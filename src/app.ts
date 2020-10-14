f425174bb8c1bc8b3d63e5eff94bca665b222233
rixong <rixong@gmail.com>
2020-10-14 16:03:46 -0500

    set up event listner

--- src/app.ts
+++ src/app.ts
@@ -6,26 +6,48 @@ description,
 number of people
 */
 
-const projects = {};
+const projects: Project[] = [];
 
 class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
 
   constructor(){
-    this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
-    this.hostElement = document.getElementById('app')! as HTMLDivElement;
+    this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;  //gets the template
+    this.hostElement = document.getElementById('app')! as HTMLDivElement; //gets the app container
 
-    const importedNode = document.importNode(this.templateElement, true);
-    this.element = importedNode.firstElementChild as HTMLFormElement;
-    this.attach()
+    const importedNode = document.importNode(this.templateElement.content, true); //clones the template
+    this.element = importedNode.firstElementChild as HTMLFormElement; //strips the form from template clone
+    this.element.id = 'user-input';
+    
+    this.titleInputElement = this.element.querySelector('#title') as HTMLInputElement;
+    this.descriptionInputElement = this.element.querySelector('#description') as HTMLInputElement;
+    this.peopleInputElement = this.element.querySelector('#people') as HTMLInputElement;
+
+    this.configure();
+    this.attach();
+  }
+
+  private submitHandler(event: Event){
+    event.preventDefault();
+    console.log(this.titleInputElement.value);
+    
+  }
+
+  private configure(){
+    this.element.addEventListener('submit', this.submitHandler.bind(this))
   }
 
   private attach (){
-    this.hostElement.insertAdjacentElement('afterbegin',this.element);
+    this.hostElement.insertAdjacentElement('afterbegin',this.element);  //add the html to app container
   }
 }
+
+
 
 class Project {
   title: string; 
@@ -37,11 +59,9 @@ class Project {
     this.description = description;
     this.numPeople = numPeople;
   }
-
 }
 
-const project = new ProjectInput();
-project.renderForm();
+const prjInput = new ProjectInput();
 
 // const fence = new Project('Repair Fence', 'Build a new back fence', 2);
 // console.log(fence.title);
