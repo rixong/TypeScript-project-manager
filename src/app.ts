30511750323cd72f258590205d59080b83ff09b5
rixong <rixong@gmail.com>
2020-10-16 11:45:31 -0500

    added dnd interfaces

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,21 @@
+
+interface Draggable {
+  dragStartHandler(event: DragEvent): void;
+  dragEndHandler(event: DragEvent): void;
+};
+
+interface DragTarget {
+  dragOverHandler(event: DragEvent): void;
+  dropHandler(event: DragEvent): void;
+  dragLeaveHandler(event: DragEvent): void;
+};
+
 /// PROJECT TYPE
 
-enum ProjectStatus { Active, Finished };
+enum ProjectStatus { 
+  Active, 
+  Finished 
+};
 
 class Project {
   constructor(
